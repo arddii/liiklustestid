850749b61b9c7d0b61e36d3a36e120f619ec6086
arddii <karpson153@gmail.com>
2022-08-12 00:33:43 +0300

    Bugfix

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -4,9 +4,9 @@
         <h4 v-else class="text-slate-900 text-xl tracking-tight font-bold mb-3">{{ index }}. {{ question.question }}</h4>
         <div>
             <div v-for="(answer, index) in question.answers" :key="index">
-                <input class="w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 focus:ring-yellow-500" type="radio" :name="question.id + '-answer'" :id="answer" :value="answer">
-                <label v-if="is_wrong && correct === answer" class="text-lg text-green-600 font-semibold px-2" :for="answer">{{ answer }}</label>
-                <label v-else class="text-lg text-gray-500 px-2" :for="answer">{{ answer }}</label>
+                <input class="w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 focus:ring-yellow-500" type="radio" :name="question.id + '-answer'" :id="question.id + answer" :value="answer">
+                <label v-if="is_wrong && correct === answer" class="text-lg text-green-600 font-semibold px-2" :for="question.id + answer">{{ answer }}</label>
+                <label v-else class="text-lg text-gray-500 px-2" :for="question.id + answer">{{ answer }}</label>
             </div>
         </div>
     </div>
